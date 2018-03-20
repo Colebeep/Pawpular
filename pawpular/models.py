f5054b3f1f607e39ec6a766d1446641a6160e013
Jill Hynes <jlhynes@umass.edu>
2018-03-20 16:37:37 -0400

    Started models.py template

---
+++ pawpular/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Example model
+class ExampleModel(models.Model):
+    """
+    Model representing a map post
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+# Create your models here.
+
+class MapPost(models.Model):
+    """
+    Model representing a map post
+    """
+
+class SocialPost(models.Model):
+    """
+    Model representing a social post
+    """
+
+
+class ServicePost(models.Model):
+    """
+    Model representing a service post
+    """
+
+class User(models.Model):
+    """
+    Model representing a user.
+    """
+
+
+class Pet(models.Model):
+    """
+    Model representing a pet.
+    """
+
+class Comment(models.Model):
+    """
+    Model representing a pet.
+    """
