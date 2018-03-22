27b36101a954bd60901b113fdc5a6c75b779ca04
Elisa Coutant <elisacoutant@comcast.net>
2018-03-22 15:52:15 -0400

    Url mappings

--- rabidSquirrels/pawpular/urls.py
+++ rabidSquirrels/pawpular/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.chat, name='chat'),
+    path('', views.map, name='map'),
+    path('', views.profile, name='profile'),
+    path('', views.services, name='services'),
+    path('', views.settings, name='settings'),
 ]
--- rabidSquirrels/pawpular/views.py
+++ rabidSquirrels/pawpular/views.py
@@ -12,3 +12,39 @@ def index(request):
         'index.html',
         context={},
     )
+
+def chat(request):
+    return render(
+        request,
+        'chat.html',
+        context={},
+    )
+
+def map(request):
+    return render(
+        request,
+        'map.html',
+        context={},
+    )
+
+def profile(request):
+    return render(
+        request,
+        'profile.html',
+        context={},
+    )
+
+def services(request):
+    return render(
+        request,
+        'services.html',
+        context={},
+    )
+
+def settings(request):
+    return render(
+        request,
+        'settings.html',
+        context={},
+    )
+
