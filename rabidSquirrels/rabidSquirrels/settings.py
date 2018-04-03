7de5005c4800ca217fbcc1a7d7f344eaaeba9c7c
stephenren <cheeryparadox@gmail.com>
2018-04-03 16:40:01 -0400

    login log out pages are placed where they should be

--- rabidSquirrels/rabidSquirrels/settings.py
+++ rabidSquirrels/rabidSquirrels/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'rabidSquirrels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- rabidSquirrels/rabidSquirrels/urls.py
+++ rabidSquirrels/rabidSquirrels/urls.py
@@ -38,3 +38,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
